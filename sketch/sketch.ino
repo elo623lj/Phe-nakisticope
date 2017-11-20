c151a2d5ae8f1baa559a0d369677b27e9b93d853
elo623lj <elo623lj@Elode-MacBook-Pro.local>
2017-11-21 01:28:41 +0800

    F

---
+++ sketch/sketch.ino
@@ -0,0 +1,17 @@
+// drawings = 60*fps/rpm
+// e.g. 10 drawings on disc require 180rpm when shooting video at 30fps
+
+#include <Stepper.h>
+int rpm; // rpm
+const int stepsPerRevolution = 200;  
+Stepper myStepper(stepsPerRevolution, 8, 9, 10, 11);
+void setup() {
+  rpm = 180; // higher rpm requires 12V DC
+  myStepper.setSpeed(rpm);
+}
+
+void loop() {
+  myStepper.step(-1); // +: CW, =:CCW
+}
+
+
